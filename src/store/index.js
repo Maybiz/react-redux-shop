d4fb174221cab644fb04cc0e4b43cf33c36d1061
Maybiz <maxime.julliant@laposte.net>
2021-04-08 14:26:24 +0200

    end app

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,11 @@ import phoneReducer from './reducers/phone.reducers'
 import tvReducer from './reducers/tv.reducers'
 import commentsReducer from './reducers/comments.reducers'
 import { createStore, combineReducers, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import thunk from 'redux-thunk'
 
 const appReducers = combineReducers({phoneReducer, tvReducer, commentsReducer})
 
-const store = createStore(appReducers, applyMiddleware(thunk))
+const store = createStore(appReducers, composeWithDevTools(applyMiddleware(thunk)))
 
 export default store
